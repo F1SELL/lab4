e820c9504809a81f5104fd15aca45cdf4d57b26d
F1SELL <85794003+F1SELL@users.noreply.github.com>
2024-11-20 12:05:00 +0300

    Update dop4.py

--- dop4.py
+++ dop4.py
@@ -1,30 +1,34 @@
 import time
-
+import importlib
+import main
+import dop1
+import dop2
+import dop3
 
 start_time = time.perf_counter()
 for i in range(100):
-    import main
+    importlib.reload(main)
 end_time = time.perf_counter()
 
 print(f"Основной- {end_time - start_time}")
 
 start_time = time.perf_counter()
 for i in range(100):
-    import dop1
+    importlib.reload(dop1)
 end_time = time.perf_counter()
 
 print(f"Доп 1  - {end_time - start_time}")
 
 start_time = time.perf_counter()
 for i in range(100):
-    import dop2
+    importlib.reload(dop2)
 end_time = time.perf_counter()
 
 print(f"Доп 2  - {end_time - start_time}")
 
 start_time = time.perf_counter()
 for i in range(100):
-    import dop3
+    importlib.reload(dop3)
 end_time = time.perf_counter()
 
 print(f"Доп 3  - {end_time - start_time}")
